c674fdc95db5c6442cb46db549dd097685979a3e
LolaInTa <lolainta@gmail.com>
2023-01-23 18:27:22 +0800

    refactor: remove db_user

--- src/sql.py
+++ src/sql.py
@@ -3,12 +3,12 @@ from mysql import connector
 class SQL():
     def __init__(self):
         try:
-            with open('/run/secrets/mysql_password','r') as f:
-                user_passwd=f.read()
+            with open('/run/secrets/mysql_root_password','r') as f:
+                root_passwd=f.read()
             self.db=connector.connect(
                 host="mysql",
-                user="db_user",
-                password=user_passwd,
+                user="root",
+                password=root_passwd,
                 port=3306
             )
         except Exception as e:
